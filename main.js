cc805b313145ea619c7c436ab187aebf9f65036a
JUSTDEEPAN <120191409+JUSTDEEPAN@users.noreply.github.com>
2023-06-21 19:54:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song1 = loadSound("song 1.mp3");
+    song2 = loadSound("song 2 (1).mp3 ");
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized!!!');
+}
+
+function gotPoses(results)
+{
+    if(results > 0)
+    {
+        console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
